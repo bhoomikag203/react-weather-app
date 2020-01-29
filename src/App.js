65733b141361a785f4dbd6ca104282783f450035
bhoomikag203 <bhoomikag203@gmail.com>
2020-01-29 13:26:40 +0530

    adding styles, final commit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ class App extends React.Component {
     const country = e.target.elements.country.value
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json()
+    console.log(data);
+
     if (city && country) {
       this.setState({
         temperature: data.main.temp,
@@ -37,7 +39,7 @@ class App extends React.Component {
         city: undefined,
         country: undefined,
         description: undefined,
-        error: "Please enter location"
+        error: "Please enter the location"
       })
     }
   }
@@ -45,16 +47,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Title />
-        <Form getWeather={this.getWeather} />
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          description={this.state.description}
-          humidity={this.state.humidity}
-          error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-sm-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    humidity={this.state.humidity}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Title = () => (
     <div>
-        <h1>Weather application</h1>
-        <p>Get access to weather data of different places...</p>
+        <h1 className="title-container__title">Weather Application</h1>
+        <p className="title-container__subtitle">Get access to weather data of different places...</p>
     </div>
 )
 
